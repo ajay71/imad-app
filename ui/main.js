f1822e1ecf897a79282975566a17daa15dd65db4
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-08-13 15:37:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,16 @@ el.innerHTML = 'Akshat Soft';
 
 // move the image
 var img = document.getElementById('madi');
+
+var marginLeft =0;
+function moveright () {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft +'px';
+    
+}
+
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveright,50);
+    
+    
 };
