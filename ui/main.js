c319b98153cd4e9becda7454bcb2f849c54bcdbd
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-08-30 18:45:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,9 +31,27 @@ var nameInput=document.getElementById('name');
 var name =nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    //make a request to the server and send the name
+    
+      // CREATE A REQUEST OBJECT
+    
+var request = new XMLHttpRequest();
+
+// Capture the response and store it in a variable
+request.onreadystatechange = function(){
+    if(request.readyState == XMLHttpRequest.DONE){
+        // take some action
+        if(request.status ==200){
+          
+          
+          
+          //  var counter = request.responseText;
+        //    var span = document.getElementById('count'//);
+        //    span.innerHTML = counter.toString();
+            
+         //make a request to the server and send the name
     //Capture list of names and render it as a list
-    var names= ['name1', 'name2', 'name3', 'name4'];
+    var names= request.responseText;
+    names =JSON.parse(names);
     var list='';
     for (var i=0;i<names.length; i++) {
         list  += '<li>'+ names[i]+'</li>';
@@ -41,6 +59,20 @@ submit.onclick=function(){
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML=list;
+            
+        }
+    }
+    //not done yet
+    
+};
+// Make the requst
+    request.open('GET', 'http://ajaysaf.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+
+
+
+   
+    
     
     
 };
