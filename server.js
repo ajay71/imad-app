c93b3a3db082cdef19c1cd9ef4fceff2194d3fef
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-07 17:34:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'ajaysaf',
+    database: 'ajaysaf',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -103,6 +114,17 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function(er, result){
+        
+        
+    });
+    
+    
 });
 
 var counter =0;
