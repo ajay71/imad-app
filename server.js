4dd6c04f8ff4622534efef770f399d2378588d68
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-08 21:55:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.get('/articles/:articleName', function (req, res)
                 
                 else
                 {
-                    if(result.row.length === 0) 
+                    if(result.rows.length === 0) 
                     {
                         res.status(404).send('Article not found');
                     } 
