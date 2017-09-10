25a89e023e6ee9d6176786d5a7e31c038ee488d2
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-10 09:56:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('crypto');
 var Pool = require('pg').Pool;
 
 var config = {
@@ -63,6 +63,22 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input, salt){
+    // how do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+    
+});
+
+ 
+
+
+
 var pool = new Pool(config);
 
 app.get('/test-db', function(req, res) {
