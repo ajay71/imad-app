e0588e106d45e3e4d9cd0e35e1e6ef0c752d7b0e
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-12 19:51:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,5 @@
-//counter code
-var button = document.getElementById('counter');
 
-button.onclick=function(){
-
-    // CREATE A REQUEST OBJECT
-    
-var request = new XMLHttpRequest();
-
-// Capture the response and store it in a variable
-request.onreadystatechange = function(){
-    if(request.readyState == XMLHttpRequest.DONE){
-        // take some action
-        if(request.status ==200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            
-        }
-    }
-    //not done yet
-};
-
-// Make the requst
-request.open('GET', 'http://ajaysaf.imad.hasura-app.io/counter', true);
-request.send(null);
-
-};
+//submit username/password to login
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
@@ -39,31 +13,22 @@ request.onreadystatechange = function(){
     if(request.readyState == XMLHttpRequest.DONE){
         // take some action
         if(request.status ==200){
-          
-       
-         //make a request to the server and send the name
-    //Capture list of names and render it as a list
-    var names= request.responseText;
-    names =JSON.parse(names);
-    var list='';
-    for (var i=0;i<names.length; i++) {
-        list  += '<li>'+ names[i]+'</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
-            
+         console.log('user logged in');
+         alert('Logged in successfully');
+        } else if(requset.status===400) {
+           alert('Username/password is incorrect');
+        } else if(request.status===500){
+            alert('server error');
         }
     }
-    //not done yet
-    
 };
-
- //submit name
-var nameInput=document.getElementById('name');
-var name =nameInput.value;
 // Make the requst
-    request.open('GET', 'http://ajaysaf.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', 'http://ajaysaf.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
 
 };
