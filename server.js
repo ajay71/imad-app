e5650cf201fa0394f5771a978c152c279ded5902
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-12 23:35:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function createTemplate (data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendfile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 function hash (input, salt){
