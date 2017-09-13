e41a48ee59e304c8d1ac19ef24be851a03418fd7
ajay71 <30769349+ajay71@users.noreply.github.com>
2017-09-13 18:23:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,10 +154,10 @@ app.get('/check-login', function(req, res){
     }
 } );
 
-app.get('/logout', funtion(req, res){
+app.get('/logout', function(req, res){
     delete req.session.outh;
     res.send('Logged out');
-});
+}); 
 
 
 var pool = new Pool(config);
